864cdd06e2b03d20fd4e4c02322f542def392179
Mongoljan Sabyrjan <mongoljansabyrjan@Mongoljans-MacBook-Pro.local>
2024-11-19 15:25:16 +0800

    changes

--- app/main/page.js
+++ app/main/page.js
@@ -3,7 +3,41 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+              <div className="flex px-[15%] justify-between h-[80px] bg-blue-500">
 
+<div className="flex">
+               <Image 
+        src="https://lh6.googleusercontent.com/proxy/uCT5REi7Ogmqno9e08qzG8ALwU6ZciWybbtNV9pa0MTCbxRBowNQMHTBWQUs4CXMkSyu7CHB5CMfLaT1OaYj19bdrrGnJUTv8vace1UEWiE" 
+        alt="Image description" 
+        width={350} 
+        height={80} 
+      />
+      <Image 
+      src="https://bida.ub.gov.mn/media/logo%20bosoo%20PNG.png"
+      alt="НБИХГ"
+      width={100}
+      height={80}
+  /> 
+  </div>
+<div className="flex justify-around">
+<div className="place-content-center">
+  <div>
+    Хакатон-2024
+
+
+  </div>
+  <div>
+    "Project Control"
+  </div>
+            
+  </div>
+ <div className="text-start w-[400px] text-[26px]">
+    Замын хөдөлгөөний нэгдсэн мэдээллийн сан
+
+ </div>
+
+</div>
+</div>
       <div className="flex px-[30%] justify-between">
         
   
@@ -29,13 +63,7 @@ export default function Home() {
             Project Control
            </div>
 
-<div className="flex justify-end mx-[15%]">
-       <a href={"/main"}> <div className="button bg-blue-400 mt-[500px]  h-[50px] w-[200px] hover:bg-blue-600 text-[20px] rounded-lg text-center place-content-center ">
-            
-  Нэвтрэх
-           </div>
-           </a>   
-           </div>
+
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['lh6.googleusercontent.com', 'bida.ub.gov.mn'], // Add your domain here
+    },
+  };
+  
+  export default nextConfig;
+  
